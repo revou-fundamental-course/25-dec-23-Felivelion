755e13da60378b14e0a175f638f7c89a7923b0ca
Felivelion <raniamazaya28@gmail.com>
2024-01-02 21:45:50 +0700

    pesan bebas

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function alert(){
+     let a = document.getElementById("input-alas").value;
+     let t = document.getElementById("input-tinggi").value;
+
+     let hasil = 0.5*a*t
+
+     console.log (hasil)
+}
